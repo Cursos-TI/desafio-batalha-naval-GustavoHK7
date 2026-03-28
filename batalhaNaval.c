a6b7c3e01961d5dd48b76732a79f0e54365d23a1
GustavoHK7 <264541803+GustavoHK7@users.noreply.github.com>
2026-03-27 23:02:28 -0300

    Fíz o nível novato do jogo batalha naval // Introduzi um tabuleiro 10x10 com, uma linha de caracteres, uma coluna de números e, dois navios já inseridos(navio 1 na horizontal e navio 2 na vertical)  para simular um jogo de batalha naval.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,7 +9,56 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    
+    // entrada de dados com a matriz "zerada", um vetor char para a linha de caracteres, um vetor inteiro para a coluna de números do 1 até o 10.
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
+    int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tabuleiro[10][10] = {0};
+    // entrada de dados para representar o tamanho do navio e o número de casas que ele irá ocupar dentro da matriz "tabuleiro".
+    int tamanho = 3;
+    int navio = 3;
+    
+    // coordenadas do navio em horizontal.
+    int linha1 = 3;
+    int coluna1 = 6;
+        
+        for (int i = 0; i < tamanho; i++) 
+        {
+        tabuleiro[linha1][coluna1 + i] = navio;  
+        }
+    
+    // coordenadas do navio em vertical.
+    int linha2 = 6;
+    int coluna2 = 1;
+        
+        for (int i = 0; i < tamanho; i++)
+        {
+            tabuleiro[linha2 + i][coluna2] = navio;
+        }
+        
+        // impressão do tabuleiro com : a linha de caracter unico, a coluna númerada de 1 a 10, e os dois navios representados pelo valor 3 e ocupando 3 casas cada um.
+        printf("### TABULEIRO DE BATALHA NAVAL ###\n");
+        printf("  ");
 
+        for (int i = 0; i < 10; i++)
+        {
+            printf("  %c", linha[i]);
+        }
+    
+        printf("\n");
+        
+        for (int i = 0; i < 10; i++) 
+        {
+            printf("%2d ", coluna[i]);
+            for (int j = 0; j < 10; j++) 
+            {
+                printf(" %d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        
+        
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
